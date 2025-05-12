24df21ccd8c8def054b10eb9de220788ff4a87a6
AntonioSalazar <antonio.salazar@live.u-tad.com>
2025-05-12 11:22:59 +0200

    Ejercicio Terminado

--- src/Main.java
+++ src/Main.java
@@ -7,24 +7,28 @@ public class Main {
 		try {
 			Scanner sc = new Scanner (System.in);
 			boolean sonIguales=false;
-			//1 - Obten la instancia con algoritmo MD5
-			MessageDigest md = null;
+			//1 - Obten la instancia con algoritmo MD5 ---DONE---
+			MessageDigest md = MessageDigest.getInstance("MD5");
 			
 			System.out.print("Introduzca la password: ");
 			byte[] pwdByte = sc.nextLine().getBytes();
-			//2 - Actualiza la instancia
+			//2 - Actualiza la instancia ---DONE---
+			
+			md.update(pwdByte);
 
-			//3 - Calcula el resumen
-			byte[] resumenPwd = null;
+			//3 - Calcula el resumen ---DONE---
+			byte[] resumenPwd = md.digest();
 			do {
 				System.out.print("Repetir password: ");
 				pwdByte = sc.nextLine().getBytes();
-				//4 - Limpia la instancia
+				//4 - Limpia la instancia ---DONE---
+				md.reset();
 				
-				//5 - Actualiza la instancia
+				//5 - Actualiza la instancia ---DONE---
+				md.update(pwdByte);
 				
-				//6 - Calcula el resumen
-				byte[] resumenPwdRepetir = null;
+				//6 - Calcula el resumen ---DONE---
+				byte[] resumenPwdRepetir = md.digest();
 				
 				sonIguales = Arrays.equals(resumenPwd,resumenPwdRepetir);
 				if (!sonIguales) {
